f18bad5b8fbf4efe9aba808b98196c9441e485f7
Richard Gomes <rgomes.info@gmail.com>
2017-01-13 23:20:41 +0000

    init.el also runs user-defined org-mode initialization files.

--- init.el
+++ init.el
@@ -1,1 +1,7 @@
+;; render README.el and runs it
 (org-babel-load-file "~/.emacs.d/README.org")
+
+;; runs configuration for org files, if any.
+;; see: http://cestlaz.github.io/posts/using-emacs-26-gcal
+(mapcar 'org-babel-load-file
+  (directory-files "~/Documents/orgfiles/config" t ".+\.org"))

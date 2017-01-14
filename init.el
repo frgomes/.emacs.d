df9e41534d9196292c3ee5be49a13fea067d1b69
Richard Gomes <rgomes.info@gmail.com>
2017-01-14 15:08:58 +0000

    Employ org-plus-contrib instead of org

--- init.el
+++ init.el
@@ -1,2 +1,12 @@
-;; render README.el and runs it
+;; Make sure custom configurations NEVER pollute init.el
+;; note: custom.el is ignored in .gitignore
+(setq custom-file "~/.emacs.d/custom.el")
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+;; Generate README.el and perform configurations from it
 (org-babel-load-file "~/.emacs.d/README.org")
